d44735949a40ff6830127f433690d39c7cab95bf
estbak <es.baksai@duocuc.cl>
2023-09-02 18:07:11 -0400

    railway

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv/config'
 
-console.log(process.env.DB_HOST)
+console.log(process.env.MYSQLHOST)
 
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
     host: process.env.MYSQLHOST,
