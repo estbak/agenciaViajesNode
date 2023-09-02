603c3ad0ba9d48d839d8acb2b8d1cad2938637fe
estbak <es.baksai@duocuc.cl>
2023-09-02 16:18:10 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import router from "./routes/index.js"
+import db from "./config/db.js"
+
+
+const app = express()
+
+//conectar bd
+db.authenticate()
+    .then( () => console.log('BD conectada'))
+    .catch( error => console.log(error))
+
+//Definir puerto
+const port = process.env.PORT || 4000
+
+//habilitar pug
+app.set('view engine', 'pug')
+
+//obtener año actual
+app.use((req, res, next) => {
+    const year = new Date()
+    res.locals.actualYear = year.getFullYear()
+    res.locals.nombreSitio = "Agencia de viajes"
+    next()
+})
+
+//agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}))
+
+//definir carpeta publica
+app.use(express.static('public'))
+
+//agregar router
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+})
